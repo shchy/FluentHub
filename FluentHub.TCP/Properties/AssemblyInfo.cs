807bf6834f4fced47050f193d395b92c7d5dc19c
yokota.shuichi <yokota.shuichi@cy-com.co.jp>
2016-07-26 15:58:29 +0900

    v1.0.1.0

--- FluentHub.TCP/Properties/AssemblyInfo.cs
+++ FluentHub.TCP/Properties/AssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentHub.TCP")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentHub.TCP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0cf176fc-f984-47a4-a7e6-df1ec4c35aa1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ push.cmd
@@ -0,0 +1,6 @@
+nuget setApiKey %1 -Source https://www.nuget.org/api/v2/package
+
+nuget push FluentHub.%2.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push FluentHub.TCP.%2.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push FluentHub.UDP.%2.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push FluentHub.Serial.%2.nupkg -Source https://www.nuget.org/api/v2/package
