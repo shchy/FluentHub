86e59fb7b154f9a426f69a9c09104534992000da
yokota.shuichi <yokota.shuichi@cy-com.co.jp>
2016-10-19 19:26:34 +0900

    multiUdpApp

--- FluentHub.UDP/UDPBuilder.cs
+++ FluentHub.UDP/UDPBuilder.cs
@@ -28,6 +28,16 @@ namespace FluentHub
                 @this.MakeApp<T, Stream>(
                     new UDPFactory(host, sendPort, recvPort)
                     , x => x.BuildContextByStream());
-        }        
+        }
+
+        public static IAppBuilder<T, Stream> MakeAppByUdps<T>(
+           this ContainerBootstrap @this
+           , IEnumerable<Tuple<string, int, int>> serverInfos)
+        {
+            return
+                @this.MakeApp<T, Stream>(
+                    serverInfos.Select(ci => new UDPFactory(ci.Item1, ci.Item2, ci.Item3)).ToArray()
+                    , client => client.BuildContextByStream());
+        }
     }
 }
